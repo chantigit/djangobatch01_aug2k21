6c4ebf452c8ed94840b4bdcc382b5a78f8a87558
chantigit <chantifacts1@gmail.com>
2021-08-12 10:34:47 +0530

    Class3

--- DjangoApps/testapp1/testapp1/urls.py
+++ DjangoApps/testapp1/testapp1/urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls import url
-from .views1 import homepageview,aboutpageview
+from .views1 import homeview,processview
 
 urlpatterns = [
-    url(r'$^',homepageview),
-    url(r'home', homepageview),
-    url(r'start', homepageview),
-    url(r'about', aboutpageview),
+    url(r'$^',homeview),
+    url(r'home/', homeview,name='homel'),
+    url(r'process/', processview,name='processl'),
 ]
 
-'''
-http://localhost:8000/          => HOME PAGE REQUEST
-http://localhost:8000/home      => HOME PAGE REQUEST
-http://localhost:8000/start     => HOME PAGE REQUEST
-http://localhost:8000/about     => ABOUT PAGE REQUEST
-'''
--- DjangoApps/testapp1/testapp1/views1.py
+++ DjangoApps/testapp1/testapp1/views1.py
@@ -1,8 +1,7 @@
 
-from django.http import HttpResponse
-#view1
-def homepageview(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
-#view2
-def aboutpageview(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+from django.shortcuts import render
+
+def homeview(request):
+    return render(request,'index.html')
+def processview(request):
+    return render(request, 'process.html')
