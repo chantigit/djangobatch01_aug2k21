bd71150a28e76dfd5f12bd973170aba169901690
chantigit <chantifacts1@gmail.com>
2021-08-16 10:35:33 +0530

    Class5

--- DjangoApps/testapp1/testapp1/urls.py
+++ DjangoApps/testapp1/testapp1/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from .views1 import homeview,processview
+
 
 urlpatterns = [
-    url(r'$^',homeview),
-    url(r'home/', homeview,name='homel'),
-    url(r'process/', processview,name='processl'),
+  
 ]
 
--- DjangoApps/testapp1/testapp1/views1.py
+++
@@ -1,7 +0,0 @@
-
-from django.shortcuts import render
-
-def homeview(request):
-    return render(request,'index.html')
-def processview(request):
-    return render(request, 'process.html')
